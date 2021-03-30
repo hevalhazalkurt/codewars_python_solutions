2f9c843f0e0a1ddeea0e55287cbf8c6190a1cb12
hevalhazalkurt <hevalhazal@gmail.com>
2021-03-30 18:33:22 +0300

    Added “Human Readable Time” py file

--- 5kyuKatas/Human_Readable_Time.py
+++ 5kyuKatas/Human_Readable_Time.py
@@ -1,6 +1,12 @@
-
+# Solution 1
 def make_readable(seconds):
     h = seconds // 3600
     m = (seconds - h * 3600) // 60
     s = seconds - (h * 3600) - (m * 60)
     return f"{h:0>2d}:{m:0>2d}:{s:0>2d}"
+
+
+
+# Solution 2
+def make_readable(seconds):
+    return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
--- 5kyuKatas/Human_Readable_Time_2.py
+++
@@ -1,3 +0,0 @@
-
-def make_readable(seconds):
-    return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
