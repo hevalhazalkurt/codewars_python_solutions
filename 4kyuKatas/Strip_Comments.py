7d581a52a4bb1824bae34d69d93222150c8ebccc
hevalhazalkurt <hevalhazal@gmail.com>
2021-03-30 18:37:01 +0300

    Added “Strip Comments” py file

--- 4kyuKatas/Strip_Comments.py
+++ 4kyuKatas/Strip_Comments.py
@@ -1,4 +1,4 @@
-
+# Solution 1
 def solution(string,markers):
     s_list = string.split("\n")
     n_list = []
@@ -12,3 +12,12 @@ def solution(string,markers):
                 s = s + char
         n_list.append(s.strip())
     return "\n".join(n_list)
+
+
+
+# Solution 2
+def solution(string,markers):
+    s_list = string.split('\n')
+    for marker in markers:
+        s_list = [item.split(marker)[0].strip() for item in s_list]
+    return '\n'.join(s_list)
--- 4kyuKatas/Strip_Comments_2.py
+++
@@ -1,6 +0,0 @@
-
-def solution(string,markers):
-    s_list = string.split('\n')
-    for marker in markers:
-        s_list = [item.split(marker)[0].strip() for item in s_list]
-    return '\n'.join(s_list)
