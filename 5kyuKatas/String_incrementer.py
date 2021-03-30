b8d64aac5a17593cbbeab79607100090ea01b9e6
hevalhazalkurt <hevalhazal@gmail.com>
2021-03-30 18:40:02 +0300

    Added “String incrementer” py file

---
+++ 5kyuKatas/String_incrementer.py
@@ -0,0 +1,11 @@
+
+import re
+
+def increment_string(s):
+    number = re.findall(r'\d+', s)
+    if number:
+        s_number = number[-1]
+        s = s.rsplit(s_number, 1)[0]
+        number = str(int(s_number) + 1)
+        return s + '0' * (len(s_number) - len(number)) + number
+    return s + '1'
