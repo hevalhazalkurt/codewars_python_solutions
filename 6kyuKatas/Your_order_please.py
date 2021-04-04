ab670d61c8b6ac9384306ab3fcef0b44ac23b044
hevalhazalkurt <hevalhazal@gmail.com>
2021-04-04 17:05:33 +0300

    Added “Your order, please” py file

---
+++ 6kyuKatas/Your_order_please.py
@@ -0,0 +1,16 @@
+
+# Solution 1
+def order(sentence):
+    words = []
+    for i in range(1,10):
+        for word in sentence.split():
+            if str(i) in word:
+                words.append(word)
+    return " ".join(words)
+
+
+
+
+# Solution 2
+def order(sentence):
+    return " ".join(sorted(sentence.split(), key=lambda x: int(filter(str.isdigit, x))))
